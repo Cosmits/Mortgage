087eadd8412c373eb9ffda836f5c156fa9c83b01
olita16 <olenka161991@gmail.com>
2023-05-10 23:32:52 +0200

    add strings 10-14

--- src/js/listeners/onClickBankUL.js
+++ src/js/listeners/onClickBankUL.js
@@ -6,6 +6,13 @@ import renderBankInfo from '../utils/renderBankInfo';
 
 function onClickBankUL(e) {
   if (e.target.nodeName === 'UL') return;
+
+  const buttonName = e.target.textContent.toLowerCase();
+  if (buttonName.includes('edit') || buttonName.includes('delete')) {
+    console.log(buttonName);
+  }
+  if (e.target.nodeName === 'UL') return;
+
   const id = e.target.closest('.bankItems').dataset.id;
   renderBankInfo(findBankById(id, banks), refs);
 }
