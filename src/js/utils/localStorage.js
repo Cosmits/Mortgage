5e3873f8449c9f7be3549259cd6fbe785eef0be2
Cosmits <salge.cos@gmail.com>
2023-05-12 10:07:30 +0200

    add try catch

--- src/js/utils/localStorage.js
+++ src/js/utils/localStorage.js
@@ -3,7 +3,16 @@ function setLocalStorage(banks) {
 }
 
 function getLocalStorage() {
-  const arrFromLocal = JSON.parse(localStorage.getItem('Banks'));
+
+  const arrFromLocal = null;
+
+  try {
+    arrFromLocal = JSON.parse(localStorage.getItem('Banks'));
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+    return;
+  }
+
   if (arrFromLocal !== null) {
     banks.splice(0, banks.length);
     arrFromLocal.forEach(element => {
