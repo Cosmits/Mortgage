7297f0d1fa8079766c859a8e030b5072c6507726
Cosmits <salge.cos@gmail.com>
2023-05-13 02:57:27 +0200

    fix

--- src/js/markups/bankInfoMarkup.js
+++ src/js/markups/bankInfoMarkup.js
@@ -1,11 +1,11 @@
 function bankInfoMarkup(bank) {
   return `
     <div class="bank-info">
-        <p class="bank-info-tex">Bank name: ${bank.name}</p>
-        <p class="bank-info-tex">Interest rate: ${bank.interestRate} %</p>
-        <p class="bank-info-tex">Max loan: ${bank.maxLoan} uah</p>
-        <p class="bank-info-tex">Min payment: ${bank.minPayment} uah </p>
-        <p class="bank-info-tex">Loan-term: ${bank.loanTerm} month</p>
+        <p class="bank-info-text">Bank name: ${bank.name}</p>
+        <p class="bank-info-text">Interest rate: ${bank.interestRate} %</p>
+        <p class="bank-info-text">Max loan: ${bank.maxLoan} uah</p>
+        <p class="bank-info-text">Min payment: ${bank.minPayment} uah </p>
+        <p class="bank-info-text">Loan-term: ${bank.loanTerm} month</p>
       </div>
     `;
 }
